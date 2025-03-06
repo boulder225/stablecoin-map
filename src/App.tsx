74bffab1f648830348789baa40d277ede8890f02
enrico <enrico@MacBookPro.box>
2025-03-06 21:10:04 +0100

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ConcordiumWardleyMap from './stablecoin-ma';
+import ConcordiumWardleyMap from './stablecoin-map';
 
 function App() {
   return (
